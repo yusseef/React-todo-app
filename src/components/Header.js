635d3680b1c2cef98417f0dbf897d7b8e79b9649
yusseef <yussef.raouf11@gmail.com>
2021-03-19 15:37:29 +0200

    CSS styling

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+    const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Task tracker</h1>
+            <button className='btn'>Add</button>
+        </header>
+    )
+}
+
+export default Header
