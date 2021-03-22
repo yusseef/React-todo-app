65ee5e8b48937b6c6f612bdbc078badb6476f83b
yusseef <yussef.raouf11@gmail.com>
2021-03-23 01:27:03 +0200

    add tasks,buttons components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 function App() {
+  const [tasks,setTasks] = useState(
+    [{id:1,
+    text: 'Study',
+    day: 'Mar 17',
+    reminder: true,
+    },
+    {id:2,
+        text: 'LOL',
+        day: 'Mar 18',
+        reminder: true,
+    },
+    {id:3,
+        text: 'Sleep',
+        day: 'Mar 19',
+        reminder: false,
+        }]
+        )
   return (
     <div className='container'>
-    <Header />
+    <Header title='Task tracker'/>
+    <Tasks tasks={ tasks }/>
     </div>
   );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-    const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({ title }) => {
+    const onClick = () =>{
+        console.log('Click')
+
+    }
     return (
         <header className='header'>
-            <h1>Task tracker</h1>
-            <button className='btn'>Add</button>
+            <h1>{ title }</h1>
+            <Button color='green' text= 'Hello' onClick={onClick} />
         </header>
     )
+}
+
+Header.defaultProps= {
+    title : 'Task tracker',
+}
+
+Header.propTypes= {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
